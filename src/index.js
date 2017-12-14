8287e827b7d320f3d65ece5023a8e2790b55652f
Vasyl Zadorozhnyy <amdpaxton@gmail.com>
2017-12-14 18:58:28 +0200

    modified modules and plugins

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('./assets/stylesheets/styles.scss');
+
+class Car {
+    manufacturer(car) {
+        document.write(`I have a ${car}`)
+    }
+}
+
+const bmw = new Car;
+
+
+bmw.manufacturer('bmw');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,77 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
 
 let config = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, './public'),
         filename: 'build.js'
-    }
+    },
+    module: {
+        rules: [ //loader rules
+            {
+                test: /\.scss$/,
+                use: ['css-hot-loader']
+                    .concat(ExtractTextWebpackPlugin
+                        .extract({
+                            use: ['css-loader', 'sass-loader', 'postcss-loader'],
+                            fallback: 'style-loader'
+                        })),
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [
+                    /node_modules/
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]',
+                    { // images loader
+                        loader: 'image-webpack-loader',
+                        query: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            optipng: {
+                                optimizationLevel: 4,
+                            },
+                            pngquant: {
+                                quality: '75-90',
+                                speed: 3,
+                            },
+                        },
+                    }
+                ],
+                exclude: /node_modules/,
+                include: __dirname,
+            },
+        ] // end rules
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin('styles.css')
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, './public'),
+        historyApiFallback: true,
+        inline: true,
+        open: true
+    },
+    devtool: 'eval-source-map'
 }
 
 module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(), // call the uglify plugin
+        new OptimizeCSSAssets()
+    );
+}
